8878633661db618b94130a71ab7b0393a102e517
Cvijetin <vidakovic.cvijetin@gmail.com>
2019-03-24 13:11:17 +0100

    slider added, rwd update

--- script.js
+++ script.js
@@ -3,4 +3,60 @@ $(function () {
 	  var $nav = $(".navbar");
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
-  });
+	});
+	
+	function showImages(el) {
+    var windowHeight = jQuery( window ).height();
+    $(el).each(function(){
+        var thisPos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos ) {
+            $(this).addClass("fadeIn");
+        }
+    });
+}
+
+// if the image in the window of browser when the page is loaded, show that image
+$(document).ready(function(){
+    showImages('.star');
+});
+
+// if the image in the window of browser when scrolling the page, show that image
+$(window).scroll(function() {
+    showImages('.star');
+});
+
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
